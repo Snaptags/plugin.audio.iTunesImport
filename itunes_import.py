1ebd0403c16fb36b847eb5508f51be2ac1fe3707
lithium <code@hlidskialf.com>
2015-11-22 22:35:19 -0800

    import ratings from itunes

--- itunes_import.py
+++ itunes_import.py
@@ -39,7 +39,7 @@ def build_url(query):
 def parse_library_xml():
   progress = xbmcgui.DialogProgress()
   progress.create(__addonname__, 'Downloading Plist')
-  progress.update(25)
+  progress.update(1)
 
   f = xbmcvfs.File(library_xml_path, 'r')
   buffer = StringIO.StringIO()
@@ -76,33 +76,45 @@ def find_song(title='', artist='', album=''):
   song = None
   if album:
     if album not in _album_index:
-      response = json_rpc("AudioLibrary.GetSongs", properties=['title','artist','album','file'], filter={'album': album})
+      response = json_rpc("AudioLibrary.GetSongs", properties=['title','artist','album','file','rating'], filter={'album': album})
       if 'songs' in response['result']:
         a = {}
         for song in response['result']['songs']:
           a[song['title']] = song
         _album_index[album] = a
+      else:
+        # print "not found:  title={0} artist={1} album={2}".format(title, artist, album)
+        return
     song = _album_index[album].get(title, None)
   elif artist:
     if artist not in _artist_index:
-      response = json_rpc("AudioLibrary.GetSongs", properties=['title','artist','album','file'], filter={'artist': artist})
+      response = json_rpc("AudioLibrary.GetSongs", properties=['title','artist','album','file','rating'], filter={'artist': artist})
       if 'songs' in response['result']:
         a = {}
         for song in response['result']['songs']:
           a[song['title']] = song
         _artist_index[artist] = a
+      else:
+        # print "not found:  title={0} artist={1} album={2}".format(title, artist, album)
+        return
     song = _artist_index[artist].get(title, None)
-  if song is None:
-    print "Could not find title={0} artist={1} album={2}".format(title, artist, album)
+  # if song is None:
+    # print "Could not find title={0} artist={1} album={2}".format(title, artist, album)
   return song
 
 
 
 
 if screen == 'top':
+  # Import Playlist
   playlists_url = build_url({'screen': 'playlists'})
   import_playlist_item = xbmcgui.ListItem('Import Playlist', iconImage='DefaultFolder.png')
   xbmcplugin.addDirectoryItem(handle=__handle__, url=playlists_url, listitem=import_playlist_item, isFolder=True)
+
+  # Import Ratings 
+  ratings_url = build_url({'screen': 'import-ratings'})
+  ratings_item = xbmcgui.ListItem('Import Ratings', iconImage='DefaultFile.png')
+  xbmcplugin.addDirectoryItem(handle=__handle__, url=ratings_url, listitem=ratings_item, isFolder=False)
   xbmcplugin.endOfDirectory(handle=__handle__, succeeded=True)
 
 elif screen == 'playlists':
@@ -131,3 +143,25 @@ elif screen == 'import-playlist':
         i += 1
 
     new_playlist.write("NumberOfEntries={i}\nVersion=2\n".format(i=i))
+
+elif screen == 'import-ratings':
+  library = parse_library_xml()
+  progress = xbmcgui.DialogProgress()
+  progress.create(__addonname__, 'Importing Ratings')
+  progress.update(1)
+  num_tracks = float(len(library['Tracks']))
+  i = 0
+  for track in library['Tracks'].values():
+    if progress.iscanceled():
+      break
+    progress.update(int(i/num_tracks*100))
+    kind = track.get('Kind', '')
+    rating = track.get('Rating', 0)
+    if kind in ('MPEG audio file', 'Purchased AAC audio file', 'AAC audio file'):
+      song = find_song(title=track['Name'], artist=track.get('Artist',''), album=track.get('Album',''))
+      if song is not None and song.get('rating',0) != rating:
+        result = json_rpc("AudioLibrary.SetSongDetails", songid=song['songid'], rating=rating)
+    i += 1
+  progress.close()
+
+
