b4463921016a8dd013dff180c2855c972944664a
lithium <code@hlidskialf.com>
2015-11-22 20:01:46 -0800

    parse list of playlists

--- itunes_import.py
+++ itunes_import.py
@@ -3,6 +3,9 @@ import xbmcgui
 import xbmcplugin
 import urllib
 import urlparse
+import plistlib
+import StringIO
+import sys
 
 
 __base__ = sys.argv[0]
@@ -22,15 +25,22 @@ if not library_xml_path:
   library_xml_path = __addon__.getSetting("library_xml_path")
 
 
-
-print "base: {base}, handle:{handle} args:{args}".format(base=__base__, handle=__handle__, args=__args__)
+screen = __args__.get('screen', ['top'])[0]
+print "base:{base} handle:{handle} args:{args} screen:{screen}".format(base=__base__, handle=__handle__, args=__args__, screen=screen)
 
 def build_url(query):
     return "{base}?{query}".format(base=__base__, query=urllib.urlencode(query))
 
+def parse_library_xml():
+  progress = xbmcgui.DialogProgress()
+  progress.create('Foobar', 'Parsing library plist')
+  progress.update(25)
+  plist = plistlib.readPlist(library_xml_path)
+  progress.close()
+  return plist
 
-screen = __args__.get('screen', ['top'])[0]
-print "screen: {0}".format(screen)
+
+
 
 if screen == 'top':
   playlists_url = build_url({'screen': 'playlists'})
@@ -39,7 +49,16 @@ if screen == 'top':
   xbmcplugin.endOfDirectory(handle=__handle__, succeeded=True)
 
 elif screen == 'playlists':
-  url = build_url({'screen': 'foo'})
-  li = xbmcgui.ListItem('foo')
-  xbmcplugin.addDirectoryItem(handle=__handle__, url=url, listitem=li)
+  library = parse_library_xml()
+  for playlist in library.get('Playlists', []):
+    if not playlist.get('Distinguished Kind', None) and not playlist.get('Master', False): # skip internal 
+      url = build_url({'screen': 'import-playlist', 'playlistId': playlist['Playlist ID']})
+      li = xbmcgui.ListItem(playlist['Name'])
+      xbmcplugin.addDirectoryItem(handle=__handle__, url=url, listitem=li)
   xbmcplugin.endOfDirectory(handle=__handle__, succeeded=True)
+
+elif screen == 'import-playlist':
+  playlistId = int(__args__.get('playlistId')[0])
+  library = parse_library_xml()
+  playlist = next(p for p in library['Playlists'] if p['Playlist ID'] == playlistId)
+  print "playlist: {0}".format(playlist)
