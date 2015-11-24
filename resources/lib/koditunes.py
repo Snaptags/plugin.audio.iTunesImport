c27a4e9d42824b3079769027c0bff87fc236172a
lithium <code@hlidskialf.com>
2015-11-24 00:14:36 -0800

    timestamp aware caching

--- resources/lib/koditunes.py
+++ resources/lib/koditunes.py
@@ -3,7 +3,9 @@ import xbmcgui
 import plistlib
 import json
 import datetime
-
+import re
+import time
+import pytz
 
 class PlistEncoder(json.JSONEncoder):
   def default(self, obj):
@@ -30,14 +32,15 @@ class ITunesParser(object):
   def plist(self):
     if not self._plist:
       self.parse_library_xml()
+      self.finish_progress()
     return self._plist
 
   @property  
   def playlists(self):
     if not self._playlists:
-      self.start_progress()
-      if not self.load_playlist_index():
-        self.build_playlist_index(self.plist)
+      self.parse_library_xml()
+      if not self._playlists:
+        self.load_playlist_index()
       self.finish_progress()
     return self._playlists
 
@@ -53,8 +56,8 @@ class ITunesParser(object):
       self.progressDialog.close()
       self.progressDialog = None
 
-  def download_file_to_cache(self):
-    cache_file = xbmcvfs.File(self.cached_library_xml_path, 'w')
+  def download_file_to_cache(self, last_time=None):
+    cache_file = None
 
     f = xbmcvfs.File(self.library_xml_uri, 'r')
     chunk_size = 4096
@@ -64,35 +67,59 @@ class ITunesParser(object):
       chunk = f.read(chunk_size)
       if not chunk:
         break;
+
+      # scan the first chunk before writing the file to see if the file is updated since last time
+      if not cache_file: 
+        if last_time:
+          matches = re.search(r'<key>Date</key><date>([^<]+)</date>', chunk, re.IGNORECASE)
+          if matches:
+            t = matches.group(1)
+            print ("t: {0}".format(t))
+
+            chunk_time = pytz.utc.localize(datetime.datetime(*(time.strptime(t, "%Y-%m-%dT%H:%M:%SZ"))[0:6]))
+            print("chunk:{0}  last:{1}".format(chunk_time, last_time))
+            if chunk_time < last_time:
+              # file hasnt changed since last time we ran
+              return False
+        cache_file = xbmcvfs.File(self.cached_library_xml_path, 'w')
+
       cache_file.write(chunk)
       bytes_read += len(chunk)
       self.progressDialog.update(int(bytes_read/float(file_size)*self.DOWNLOAD_PROGRESS))
     f.close()
     cache_file.close()
+    return True
 
   def parse_library_xml(self):
     self.start_progress()
-    if not xbmcvfs.exists(self.cached_library_xml_path):
-      print("xml file not downloaded, downloading")
-      self.download_file_to_cache()
 
-    # TODO: check date in file to see if we need to update index
+    last_time = None
+    if xbmcvfs.exists(self.cached_library_xml_path):
+      local_last_time = datetime.datetime.fromtimestamp(xbmcvfs.Stat(self.cached_library_xml_path).st_ctime())
 
-    if not xbmcvfs.exists(self.cached_library_path):
+      local_zone = pytz.timezone('US/Pacific')
+      last_time = local_zone.localize(local_last_time)
+      print "local: {0}".format(local_last_time)
+
+    if self.download_file_to_cache(last_time):
+      # new file was downloaded, update our library cache
       self.progressDialog.update(self.DOWNLOAD_PROGRESS, "Parsing: iTunes Music Library.xml")
       cache_file = xbmcvfs.File(self.cached_library_xml_path, 'r')
       self._plist = plistlib.readPlist(cache_file)
       cache_file.close()
 
+      self.progressDialog.update(55, "Build: library index")
+      self.build_playlist_index(self._plist)
+
+      self.progressDialog.update(60, "Dump: library index")
       json_file = xbmcvfs.File(self.cached_library_path, 'w')
       json.dump(self._plist, json_file, cls=PlistEncoder)
       json_file.close()
     else:
-      self.progressDialog.update(self.DOWNLOAD_PROGRESS, "Loading cached iTunes Music Library.xml")
+      self.progressDialog.update(60, "Loading cached iTunes Music Library.xml")
       json_file = xbmcvfs.File(self.cached_library_path, 'r')
       self._plist = json.load(json_file)
       json_file.close()
-    self.finish_progress()
 
   def build_playlist_index(self, plist):
     playlists = {}
